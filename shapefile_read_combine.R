8ecab999511817e0c6b9a250040bfae6ba77097c
datkins41889 <datkins41889@gmail.com>
2022-12-14 13:13:38 -0700

    Completed pt for genet and ramet, saved files. buff=0.05, buffgenet = 0.02

--- shapefile_read_combine.R
+++ shapefile_read_combine.R
@@ -17,16 +17,16 @@ shp = shp[shp$species != "All Other Species",]
 
 shp = sf::st_cast(shp, to="MULTIPOLYGON")
 install.packages("tidyverse")
-library(tidyverse)
-install.packages("devtools")
-library(devtools)
-install_github('aestears/PlantTracker')
 
+install.packages("devtools")
+
+install_github('aestears/PlantTracker')
+library(tidyverse)
+library(devtools)
 library(plantTracker)
 
 ?trackSpp
 
-checkDat(shp, inv = inv, species = "species", site = "Site", quad = "Quadrat", year = "z_Year", geometry = "geometry")
 sf::st_is_valid(shp[338,], reason = T)
 plot(shp[338,]$geometry)
 
@@ -44,17 +44,7 @@ paste0("'",unique(shp_valid$Quadrat)[51:99], "'", collapse = ",")
 round_1 = c('Q28','Q61','Q62','Q63','Q64','Q66','Q67','Q68','Q97','Q80','Q37','Q26','Q10','Q34','Q6','Q54','Q55','Q56','Q70','Q25','Q48','Q29','Q7','Q42','Q43','Q44','Q45','Q89','Q91','Q92','Q93','Q94','Q95','Q96','Q98','Q9','Q79','Q81','Q82','Q83','Q85','Q86','Q87','Q88','Q494','Q498','Q30','Q31','Q32','Q33')
 round_2 = c('Q38','Q39','Q53','Q57','Q59','Q60','Q41','Q46','Q69','Q71','Q72','Q73','Q74','Q75','Q77','Q78','Q24','Q27','Q49','Q50','Q51','Q11','Q12','Q13','Q15','Q16','Q17','Q18','Q20','Q22','Q23','Q21','Q2','Q3','Q8','Q65','Q90','Q84','Q35','Q58','Q5','Q40','Q47','Q76','Q52','Q14','Q106','Q4','Q19')
 
-lambdas = trackSpp(shp_join, 
-                           inv = inv,
-                           dorm = 1,
-                           buff = 0.05,
-                           clonal = T,
-                           buffGenet = 0.02,
-                           species = "species",
-                           site = "Site",
-                           quad = "Quadrat",
-                           year = "z_Year",
-                           geometry = "geometry")
+
 lambdas = trackSpp(shp_valid[shp_valid$Quadrat %in% round_1,], 
                    inv = inv[[i]],
                    dorm = 1,
@@ -67,19 +57,18 @@ lambdas = trackSpp(shp_valid[shp_valid$Quadrat %in% round_1,],
                    year = "z_Year",
                    geometry = "geometry")
 
-quadList = list()
-i=15
-for(i in 99:length(unique(shp_valid$Quadrat))){
+quadList_ramet = list()
+i=1
+for(i in 1:length(unique(shp_valid$Quadrat))){
   dat_i = shp_valid[shp_valid$Quadrat == unique(shp_valid$Quadrat)[i],]
-  inv_i = rep(list(unique(dat_i$z_Year)), length(unique(dat_i$Quadrat)))
+  inv_i = list(c(2002:2007,2009:2021))
   names(inv_i) = unique(dat_i$Quadrat)
-  inv_i = as.list(inv_i)
-  quadList[[i]] = trackSpp(dat_i, 
+  quadList_ramet[[i]] = trackSpp(dat_i, 
                          inv = inv_i,
                          dorm = 1,
                          buff = 0.05,
-                         clonal = T,
-                         buffGenet = 0.02,
+                         clonal = F,
+                         #buffGenet = 0.02,
                          species = "species",
                          site = "Site",
                          quad = "Quadrat",
@@ -88,20 +77,8 @@ for(i in 99:length(unique(shp_valid$Quadrat))){
   
 }
 
-## Save which quads are working
-i=1
-names = list()
-for(i in 1:length(quadList)){
-  names[i] = unique(quadList[[i]]$Quadrat)
-}
-
-quadList_success <- names(quadList)
-
-quadList_success = quadList_success[quadList_success != "NULL"]
-
-quadList_clean = quadList[quadList != "NULL"]
-quadList_clean = lapply(quadList_clean,sf::st_cast, to="MULTIPOLYGON")
-pt_out = sf::st_as_sf(data.table::rbindlist(quadList_clean))
+quadList_ramet = lapply(quadList_ramet,sf::st_cast, to="MULTIPOLYGON")
+pt_out_ramet = sf::st_as_sf(data.table::rbindlist(quadList_ramet))
 
 
 plot(log(size_tplus1) ~ log(basalArea_genet), data = pt_out)  
